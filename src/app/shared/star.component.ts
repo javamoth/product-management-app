3dd26dd717e89899c69c28b4303fa139e6504055
Java Moth <javamoth@yahoo.com>
2023-09-18 07:17:48 -0600

    Minor changes

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { Component, EventEmitter, Input, Output, OnChanges, SimpleChanges } from "@angular/core";
 
 @Component({
     selector: 'pm-star',
@@ -6,11 +6,19 @@ import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
     styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnChanges{
+    
     ngOnChanges(): void {
         this.cropWidth = this.rating * 75/5;
+        
     }   
             
-     
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
     @Input() rating: number = 0;
     cropWidth: number = 75;
+
+    onClick(): void {
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
     }
+    }
+
+
