30de3949513e4568aba6a04719087ed4748032dc
Java Moth <javamoth@yahoo.com>
2023-09-19 07:26:47 -0600

    fetching data via get request

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,52 +1,61 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css']
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
+export class ProductListComponent implements OnInit, OnDestroy {
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  errorMessage: string = '';
+  sub!: Subscription;
 
-export class ProductListComponent implements OnInit{
+  private _listFilter: string = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter:', value);
+    this.filteredProducts = this.performFilter(value);
+  }
 
-    pageTitle: string = 'Product List';
-    imageWidth: number = 50;
-    imageMargin: number = 2;
-    showImage: boolean = false;
-    
-    private _listFilter: string = '';
-    get listFilter(): string {
-      return this._listFilter;
-    }
+  filteredProducts: IProduct[] = [];
+  products: IProduct[] = [];
 
-    set listFilter(value: string) {
-      this._listFilter = value;
-      console.log('In setter:', value);
-      this.filteredProducts = this.performFilter(value);
-    }
+  constructor(private productService: ProductService) { }
 
-    filteredProducts: IProduct[] = [];
-    products: IProduct[] = [];
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBy));
+  }
 
-      toggleImage(): void {
-        this.showImage = !this.showImage;
-      }
-      ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;        
-    }
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 
-    onRatingClicked(message: string): void {
-      this.pageTitle = 'Product List: ' + message;
-    }
+  ngOnInit(): void {
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
 
-    performFilter(filterBy: string): IProduct[] {
-      filterBy = filterBy.toLocaleLowerCase();
-      return this.products.filter((product: IProduct) =>
-        product.productName.toLocaleLowerCase().includes(filterBy));
-    }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
-    constructor (private productService: ProductService) {}
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
 }
-
